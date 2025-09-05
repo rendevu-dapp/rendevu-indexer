3ec917fface008c55ea1ba84bc73c338c3f7e1cb
Nathaniel Quansah <n.bongo40@gmail.com>
2025-09-05 09:14:17 +0000

    fixed build errors

--- src/common/configs/typesense.ts
+++ src/common/configs/typesense.ts
@@ -4,7 +4,7 @@ import Typesense from "typesense";
 export const typesense = new Typesense.Client({
   nodes: [
     {
-      url: process.env.TYPESENSE_URL,
+      url: process.env.TYPESENSE_URL!,
     },
   ],
   apiKey: process.env.TYPESENSE_API_KEY || "supersecret",
